dbfa3f2c2414bf28f7649c45145cf75a0b9a13ee
Ayoub Karafi <karafi.work@gmail.com>
2021-12-22 18:36:54 +0100

    error and print implemented...

---
+++ output/output.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   output.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: akarafi <akarafi@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/22 16:37:38 by akarafi           #+#    #+#             */
+/*   Updated: 2021/12/22 16:55:48 by akarafi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../push_swap.h"
+
+void	putstr_fd(char *s, int fd)
+{
+	while (*s)
+		write(fd, s++, 1);
+}
+
+void	error(void)
+{
+	putstr_fd("Error\n", 2);
+}
+
+void	print(char *s)
+{
+	putstr_fd(s, 1);
+	putstr_fd("\n", 1);
+}
