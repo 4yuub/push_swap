2fda56ef2fcb2cb9da4ead556be2cd506ddd4241
Ayoub Karafi <karafi.work@gmail.com>
2021-12-22 21:34:08 +0100

    mistake ;)

--- push_swap.c
+++ push_swap.c
@@ -6,12 +6,21 @@
 /*   By: akarafi <akarafi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/22 16:36:46 by akarafi           #+#    #+#             */
-/*   Updated: 2021/12/22 21:01:33 by akarafi          ###   ########.fr       */
+/*   Updated: 2021/12/22 21:33:47 by akarafi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 #include <stdio.h>
+
+void	print_all(t_node *a)
+{
+	while (a)
+	{
+		printf("%ld\n", a->number);
+		a = a->next;
+	}
+}
 
 int	main(int ac, char **av)
 {
@@ -23,7 +32,9 @@ int	main(int ac, char **av)
 	if (ac < 2)
 		return (0);
 	error = false;
+	garbage = NULL;
 	init(&a, &b, av, &garbage);
 	// code here
+	print_all(a);
 	free_all(&garbage, &a, &b);
 }
