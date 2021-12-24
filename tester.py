445b23c6831ca51ea1473542525842b06278ea27
Ayoub Karafi <karafi.work@gmail.com>
2021-12-24 15:09:52 +0100

    max of 900 action in 100 number

--- tester.py
+++ tester.py
@@ -9,12 +9,24 @@ class bcolors:
 
 n = int(input("number of args\n>>> "))
 print(bcolors.OKCYAN + "testing with " + str(n) + " numbers:")
-for _ in range(500):
+sum = 0
+max = 0
+min = 1_000_000
+for _ in range(1000):
 	args = subprocess.getoutput(f'ruby -e "puts (1..{n}).to_a.shuffle" | tr "\n" " "')
 	p1 = subprocess.getoutput(f"./push_swap {args} | ./checker {args}")
 	p2 = subprocess.getoutput(f"./push_swap {args} | wc -l")
 	if p1 == "OK":
+		sum += int(p2)
+		if int(p2) > max:
+			max = int(p2)
+		if int(p2) < min:
+			min = int(p2)
 		print(bcolors.OKGREEN + p1 + ": " + p2)
 	else:
 		print(bcolors.WARNING + p1, "args:", args)
-	sleep(.1)
+	#sleep(.1)
+
+print("the average is:", sum / 1000)
+print("the min is:", min)
+print("the max is:", max)
