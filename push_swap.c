0890d5fbc1f236715b621b78dbd6d6d25fdbb2d2
Ayoub Karafi <karafi.work@gmail.com>
2021-12-25 07:29:27 +0100

    remove some debuging lines of code

--- push_swap.c
+++ push_swap.c
@@ -6,21 +6,12 @@
 /*   By: akarafi <akarafi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/22 16:36:46 by akarafi           #+#    #+#             */
-/*   Updated: 2021/12/24 02:13:26 by akarafi          ###   ########.fr       */
+/*   Updated: 2021/12/25 07:29:01 by akarafi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 #include <stdio.h>
-
-void	print_all(t_node *a)
-{
-	while (a)
-	{
-		printf("%d\n", a->number);
-		a = a->next;
-	}
-}
 
 int	main(int ac, char **av)
 {
@@ -32,7 +23,6 @@ int	main(int ac, char **av)
 	if (ac < 2)
 		return (0);
 	init(&a, &b, av, &garbage);
-	// code here
 	size = get_size(a);
 	if (size <= 3)
 		sort3(&a, size);
@@ -40,9 +30,5 @@ int	main(int ac, char **av)
 		sort5(&a, &b, size);
 	else
 		sort(&a, &b, size, &garbage);
-	//print("a: ");
-	//print_all(a);
-	//print("b: ");
-	//print_all(b);
 	free_all(&garbage, &a, &b);
 }
